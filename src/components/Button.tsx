22a0e928f310597a072a1296e60381bb77811a41
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-02-25 01:07:14 +0000

    chore: update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,37 @@
 import React, { forwardRef } from 'react';
+import classname from 'classnames';
 
 type NativeButton = React.ButtonHTMLAttributes<HTMLButtonElement> &
     React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 interface ButtonProps extends NativeButton {
     skin?: 'gray' | 'indigo';
+    className?: string;
     children: React.ReactNode;
 }
 
-const Button = forwardRef(({
-    skin = 'gray',
-    children,
-    ...otherProps
-}: ButtonProps, ref: React.Ref<HTMLButtonElement & HTMLAnchorElement>) => {
-    const { href } = otherProps;
-    const TAG = href ? 'a' : 'button';
+const Button = forwardRef(
+    (
+        { skin = 'gray', className, children, ...otherProps }: ButtonProps,
+        ref: React.Ref<HTMLButtonElement & HTMLAnchorElement>
+    ) => {
+        const { href } = otherProps;
+        const TAG = href ? 'a' : 'button';
 
-    return (
-        <TAG
-            {...otherProps}
-            ref={ref}
-            type="button"
-            className={`inline-flex rounded-full p-3 cursor-pointer focus:outline-none hover:bg-${skin}-100 dark:hover:bg-${skin}-800 dark:text-white`}
-        >
-            {children}
-        </TAG>
-    );
-});
+        return (
+            <TAG
+                {...otherProps}
+                ref={ref}
+                type="button"
+                className={classname(
+                    `inline-flex rounded-full p-3 cursor-pointer focus:outline-none hover:bg-${skin}-100 dark:hover:bg-${skin}-800 dark:text-white`,
+                    className
+                )}
+            >
+                {children}
+            </TAG>
+        );
+    }
+);
 
 export default Button;
