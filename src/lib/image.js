e8f27d2711a0f0bf17aff5f6e6c612ae129fdb93
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-03-05 23:07:20 +0000

    chore: update image texture

--- src/lib/image.js
+++ src/lib/image.js
@@ -10,9 +10,9 @@ export const Board = ({ children, ...otherProps }) => (
             width: 2048,
             height: 1170,
             backgroundColor: '#fff',
-            backgroundSize: '60px 60px',
+            backgroundSize: '50px 50px',
             backgroundImage:
-                'radial-gradient(circle at 50% 50%, #ddd, #ddd 1px, transparent 1px), radial-gradient(circle at 0 0, #ddd, #ddd 1px, transparent 1px), radial-gradient(circle at 0 100%, #ddd, #ddd 1px, transparent 1px), radial-gradient(circle at 100% 0, #ddd, #ddd 1px, transparent 1px), radial-gradient(circle at 100% 100%, #ddd, #ddd 1px, transparent 1px)',
+                'radial-gradient(circle at 50% 50%, #f0f0f0, #f0f0f0 3px, transparent 3px), radial-gradient(circle at 0 0, #f0f0f0, #f0f0f0 3px, transparent 3px), radial-gradient(circle at 0 100%, #f0f0f0, #f0f0f0 3px, transparent 3px), radial-gradient(circle at 100% 0, #f0f0f0, #f0f0f0 3px, transparent 3px), radial-gradient(circle at 100% 100%, #f0f0f0, #f0f0f0 3px, transparent 3px)',
             color: '#000',
             ...otherProps.style,
         }}
