f0685c0a3b1c4d17528aaddbd490c9d20200a521
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-03-05 22:38:03 +0000

    chore: update images

--- src/images/build-button-component-in-react.js
+++ src/images/build-button-component-in-react.js
@@ -10,8 +10,8 @@ export default () => (
                 viewBox="-11.5 -10.232 23 20.463"
             >
                 <defs />
-                <circle r="2.05" fill="currentColor" />
-                <g fill="none" stroke="currentColor">
+                <circle r="2.05" fill="#61dafb" />
+                <g fill="none" stroke="#61dafb">
                     <ellipse rx="11" ry="4.2" />
                     <ellipse rx="11" ry="4.2" transform="rotate(60)" />
                     <ellipse rx="11" ry="4.2" transform="rotate(120)" />
