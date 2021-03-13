339159cb7714b4242382b95b9d335f799fed6656
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-03-13 00:02:01 +0000

    chore: update post

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import Button from '@/components/Button';
 import Alert from '@/components/Alert';
+import Anchor from '@/components/Anchor';
 
 const MDXComponents = {
     Image,
     Button,
     Alert,
+    Anchor,
 };
 
 export default MDXComponents;
